76ce5558d3c9560afb8fc6ded60dd19e96c184d1
huluobodiaoyu <1127075036@qq.com>
2024-06-22 14:49:47 +0800

    ✨ feat(types): 添加类型

--- components/_types/index.ts
+++ components/_types/index.ts
@@ -5,7 +5,7 @@ export interface ICommonProps {
   size: 'default' | 'large' | 'small',
   type: 'default' | 'primary'| 'success'| 'info'| 'warning'| 'banger',
   disabled : boolean,
-  loading:boolean,
+  loading:boolean
 }
 
 
