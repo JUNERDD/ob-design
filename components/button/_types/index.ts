5526a27cd431a2aae4cf64e4ac97cd39d0943151
JUNER <juneruse@foxmail.com>
2024-06-22 14:45:13 +0800

    ✨ feat(button): 初始化按钮

---
+++ components/button/_types/index.ts
@@ -0,0 +1,6 @@
+import type { ICommonProps } from '../../_types'
+
+/**
+ * Button props - 按钮
+ */
+export interface IButtonProps extends ICommonProps { }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  dir: {
-    pages: 'docs',
-  },
   modules: [
-    '@nuxt/eslint', '@unocss/nuxt',
+    '@nuxt/eslint',
+    '@unocss/nuxt',
   ],
   css: ['@unocss/reset/tailwind-compat.css'],
   eslint: {
     checker: true,
     config: {
       stylistic: true,
-      standalone: false
+      standalone: false,
     },
   },
 })
