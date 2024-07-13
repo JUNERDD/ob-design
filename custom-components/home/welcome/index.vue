ec57d35254593f3fb826e4bc06e8af08161bb0cd
JUNER <53842108@qq.com>
2024-07-14 00:47:07 +0800

    📦 file(spline): 更换首页3d模型

--- custom-components/home/welcome/index.vue
+++ custom-components/home/welcome/index.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
-import splineWelcomeUrl from '@/assets/spline/welcome_url.splinecode'
+import splineWelcome from '@/assets/spline/welcome.splinecode'
 
 defineOptions({ name: 'HomeWelcome' })
 </script>
 
 <template>
   <div class="h-4xl w-full flex-center lt-lg:h-2xl lt-md:h-md">
-    <ObSpline3d :scene-url="splineWelcomeUrl" />
+    <ObSpline3d :scene-url="splineWelcome" class="animate-zoom-in-down" />
   </div>
 </template>
 
 <style scoped>
-
 </style>
