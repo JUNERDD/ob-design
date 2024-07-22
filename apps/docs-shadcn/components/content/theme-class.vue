c685b02ba16868c34495a3261760a43e0e3d45a1
JUNER <53842108@qq.com>
2024-07-22 18:00:48 +0800

    📝 docs(components): 初始化主题配置

---
+++ apps/docs-shadcn/components/content/theme-class.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ObButton, ObLink, ObSpin, ObTabGroup, ObTheme } from '@obdesign/components'
+</script>
+
+<template>
+  <div class="flex flex-wrap items-center gap-3">
+    <!-- 传递主题props -->
+    <ObTheme class="max-w-120 bg-red p-5 c-white">
+      <ObButton>Hello World</ObButton>
+      <ObLink>Hello World</ObLink>
+      <ObSpin />
+    </ObTheme>
+  </div>
+</template>
