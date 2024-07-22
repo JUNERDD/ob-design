ea4b4b877ce6634bc01079489766b07aeec36669
JUNER <53842108@qq.com>
2024-07-22 22:10:44 +0800

    📝 docs(components): 调整组件文档部分内容

--- apps/docs-shadcn/components/content/theme-class.vue
+++ apps/docs-shadcn/components/content/theme-class.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
-import { ObButton, ObLink, ObTabGroup, ObTheme } from '@obdesign/components'
+import { ObButton, ObLink, ObTheme } from '@obdesign/components'
 </script>
 
 <template>
   <div class="flex flex-wrap items-center gap-3">
+    <NuxtPage />
     <!-- 传递主题props -->
     <ObTheme class="max-w-120 bg-red p-5 c-white">
-      <ObButton>Hello World</ObButton>
-      <ObLink>Hello World</ObLink>
+      <ObButton>我是button</ObButton>
+      <ObLink>我是link，想不到吧</ObLink>
     </ObTheme>
   </div>
 </template>
