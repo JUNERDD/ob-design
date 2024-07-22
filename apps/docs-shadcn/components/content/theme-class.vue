bcb1138c52fda9a6a40080e62cc7463c25066a03
JUNER <53842108@qq.com>
2024-07-22 22:11:35 +0800

    📝 docs(components): 调整

--- apps/docs-shadcn/components/content/theme-class.vue
+++ apps/docs-shadcn/components/content/theme-class.vue
@@ -6,7 +6,7 @@ import { ObButton, ObLink, ObTheme } from '@obdesign/components'
   <div class="flex flex-wrap items-center gap-3">
     <NuxtPage />
     <!-- 传递主题props -->
-    <ObTheme class="max-w-120 bg-red p-5 c-white">
+    <ObTheme class="max-w-120 bg-red p-5 text-white">
       <ObButton>我是button</ObButton>
       <ObLink>我是link，想不到吧</ObLink>
     </ObTheme>
