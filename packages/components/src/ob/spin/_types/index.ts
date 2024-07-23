4f8c60325acad8c991358551fdeee28719e7c762
JUNER <53842108@qq.com>
2024-07-23 18:35:40 +0800

    🔨 fix(loading-box): 修复加载圈偏移问题

--- packages/components/src/ob/spin/_types/index.ts
+++ packages/components/src/ob/spin/_types/index.ts
@@ -4,7 +4,11 @@ import type { ICommonProps } from '../../../index'
 /**
  * ObSpin - 参数类型
  */
-export interface ISpinProps extends Omit<ICommonProps, 'loading'> {
+export interface ISpinProps extends Omit<ICommonProps, 'loading' | 'class'> {
+  /**
+   * 盒子样式
+   */
+  boxClass?: ClassValue
   /**
    * 线条样式
    */
