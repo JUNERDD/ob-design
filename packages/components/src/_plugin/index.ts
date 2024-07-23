dcad73b040045f2073a52b164ffda81a8f303bf0
JUNER <53842108@qq.com>
2024-07-23 14:23:38 +0800

    🔧 config(ts): 初步编写类型声明文件

--- packages/components/src/_plugin/index.ts
+++ packages/components/src/_plugin/index.ts
@@ -1,36 +1,25 @@
-import type { App } from 'vue'
-// import Spin from 'obdesign'
+import type { App, DefineComponent } from 'vue'
+import * as ob from '../ob'
+import { isComponent } from '../_tools'
+
+// 注册参数
+interface IPluginPayload {
+  prefix: string
+}
 
 /**
- * Ob-Design vue 插件
+ * 挂载全局组件插件
  */
-export function ObDesignPlugin(app: App<Element>) {
-  const getClass = (value: boolean) => value ? ['before:visible', 'before:bg-red'] : ['before:invisible', 'before:bg-black', 'before:opacity-0']
+export default function obdesign(app: App, payload?: IPluginPayload) {
+  const { prefix = 'Ob' } = payload || {}
 
-  app.directive('loading', {
-    created(el: HTMLElement, { value }) {
-      if (value) {
-        el.classList.add('loading', ...getClass(value))
-      }
-    },
-    // mounted(el: HTMLElement, { value }) {
-    //   const ele = document.createElement('div')
+  Object.entries(ob).forEach(([key, value]) => {
+    if (isComponent(value)) {
+      // 替换原有的前缀
+      const cpnName = key.replace('Ob', '')
 
-    //   render(h(Spin), ele)
-
-    //   el.append(ele)
-    // },
-    updated(el: HTMLElement, { value }) {
-      // 删除另外一个值的class
-      el.classList.remove(...getClass(!value))
-
-      // 添加当前值的class
-      el.classList.add(...getClass(value))
-    },
-    getSSRProps({ value }) {
-      return {
-        class: `loading-box ${getClass(value).join(' ')}`,
-      }
-    },
+      // 注册组件
+      app.component(prefix + cpnName, (value as DefineComponent))
+    }
   })
 }
--- packages/components/types.d.ts
+++ packages/components/types.d.ts
@@ -1,17 +1,3 @@
-export * from './src/index'
+/// <reference types="./declare/global.d.ts" />
 
-declare module '@vue/runtime-core' {
-  export interface GlobalComponents {
-    ObButton: typeof import('./src/ob/button/index.vue')['default']
-    ObCard: typeof import('./src/ob/card/index.vue')['default']
-    ObLink: typeof import('./src/ob/link/index.vue')['default']
-    ObLoadingBox: typeof import('./src/ob/loading-box/index.vue')['default']
-    ObSpin: typeof import('./src/ob/spin/index.vue')['default']
-    ObTabGroup: typeof import('./src/ob/tab-group/index.vue')['default']
-    ObTheme: typeof import('./src/ob/theme/index.vue')['default']
-    ObModal: typeof import('./src/ob/modal/index.vue')['default']
-    ObSection: typeof import('./src/ob/section/index.vue')['default']
-    ObSectionBlock: typeof import('./src/ob/section/block.vue')['default']
-    ObSpline3d: typeof import('./src/ob/spline-3d/index.vue')['default']
-  }
-}
+export * from './src'
